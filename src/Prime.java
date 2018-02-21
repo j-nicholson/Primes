356666fbeb74ae8b8dc44e96b9fb85207f9e9468
j-nicholson <johnfnicholson123@gmail.com>
2018-02-21 13:54:01 -0500

    Documented method

--- src/Prime.java
+++ src/Prime.java
@@ -9,21 +9,25 @@ public class Prime {
 		long input = scan.nextLong();
 		System.out.println(isPrime(input));
 		scan.close();
-		
 	}
 	
+	/**
+	 * Tests if an integer is prime or composite
+	 * @param n integer to be tested
+	 * @return boolean whether the integer tested is prime [true] or composite [false]
+	 */
 	public static boolean isPrime(long n) {
 		if(n == 1) {
 			return false;
 		}
 		
 		double testLimit = Math.sqrt(n);
+		
 		for(int i = 2; i <= testLimit; i++) {
 			if(n % i == 0) {
 				return false;
 			}
 		}
-		
 		return true;
 	}
 }
